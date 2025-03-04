4f67bc07e691f22f8d9e86632631d154adc09015
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-03-04 14:56:55 +0600

    Theme changed

--- frontend/tailwind4/src/components/NavBar.jsx
+++ frontend/tailwind4/src/components/NavBar.jsx
@@ -11,14 +11,14 @@ const NavBar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
+                        <li><a>Home</a></li>
+                        <li><a>Dashboard</a></li>
+                        <li><a>About Us</a></li>
                     </ul>
                 </div>
             </div>
             <div className="navbar-center">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Smart Spend</a>
             </div>
             <div className="navbar-end">
                 <button className="btn btn-ghost btn-circle">
